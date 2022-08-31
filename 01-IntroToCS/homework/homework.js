2e4534df5fe5aa4739e4dbaa11db5041760f022a
sofia-arias <sofi_100_15@hotmail.com>
2022-08-30 22:15:16 -0300

    tarea finalizada

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -8,6 +8,22 @@ function BinarioADecimal(num) {
   }
   return resul;
 }
+/*
+function BinarioADecimal(num) {
+  // tu codigo aca
+  // input: string --> "10" -->  (2**1)*1     + (2**0)*0 == 2
+  // output: num --> 2
+  let sum = 0;
+  let pos = 0;
+  let arr = num.split("");
+​
+  for (let i = arr.length - 1; i >= 0; i--) {
+    sum += arr[i] * Math.pow(2, pos);
+    pos++;
+  }
+  return sum;
+}
+*/
 
 function DecimalABinario(num) {
   // tu codigo aca
