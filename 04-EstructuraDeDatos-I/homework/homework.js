cfda5e4aad6c4afb5529152893f5208158c5bb4a
sofia-arias <sofi_100_15@hotmail.com>
2022-09-05 16:20:07 -0300

    finalizada

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -22,6 +22,8 @@ function nFactorial(n) {
 }
 
 function nFibonacci(n) {
+  //if (n ===0)return 0;cualquiera de estas dos opciones esta bien tmb
+  // if(n === 1)return 1;
   if(n>=0 && n<2)return n;
   return nFibonacci(n-1)+ nFibonacci(n-2)
 }
