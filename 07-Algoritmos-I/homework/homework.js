4af4a2c71d163eb65a98d3459545e18119f9ef45
sofia-arias <sofi_100_15@hotmail.com>
2022-09-07 13:07:11 -0300

    listo

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -24,17 +24,17 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  var flag = true;
-  while(flag){
-    flag = false;
-for (var i = 0; i < array.length - 1; i++){
-  if(array[i] > array[i + 1]){
-    var aux= array[i]
-    array[i] = array[i + 1];
-    array[i + 1] = aux
-    flag = true
-  }
-
+  let swap = true;
+  while(swap){
+    swap=false;
+    for(let i = 0; i < array.length -1; i++){
+      if(array[i]>array[i+1]){
+        let aux = array[i]
+        array[i] = array[i+1];
+        array[i+1]=aux;
+        swap=true;
+      }
+    }
   }
   return array;
 }
@@ -45,7 +45,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i = 1; i < array.length;i++){
+    let j = i-1;
+    let aux= array[i];
+    while(j>=0 && aux < array[j]){
+      array[j+1] = array[j];
+      j--;
+    }
+    array[j+1]=aux;
+  }
+  return array;
 }
 
 
@@ -54,7 +63,21 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for(let i = 0; i < array.length; i++){
+    let min = i;
 
+    for(let j=i+1; j < array.length; j++){
+      if(array[j] < array[min]){
+        min = j;
+      }
+    }
+    if(i !== min){
+      let aux = array[i];
+      array[i]=array[min];
+      array[min]=aux;
+    }
+  }
+  return array;
 }
 
 
